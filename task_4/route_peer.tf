ac3fc8e8d6f8154e3cb73596b6d28656ddc5d89f
EC2 Default User <betulakman0167@gmail.com>
2020-05-03 18:55:08 +0000

    added

--- task_4/route_peer.tf
+++ task_4/route_peer.tf
@@ -1,18 +1,15 @@
 
+resource "aws_route" "r"{
+    route_table_id  = "${aws_route_table.dev.id}"
+    destination_cidr_block = "${var.cidr_block}" 
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
+}
 
-
-
-# resource "aws_route" "r"{
-#     route_table_id  = "${aws_route_table.dev.id}"
-#     destination_cidr_block = "${var.cidr_block}" 
-#     vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
-# }
-
-# resource "aws_route" "r1"{
-#     route_table_id  = "${aws_route_table.pr.id}"
-#     destination_cidr_block = "${var.cidr_block}" 
-#     vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
-# }
+resource "aws_route" "r1"{
+    route_table_id  = "${aws_route_table.pr.id}"
+    destination_cidr_block = "${var.cidr_block}" 
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
+}
 
 
 
